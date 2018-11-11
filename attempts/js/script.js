f69cd1dfa0af2b1bc759ae7020e6f0c36f1f61a8
Victor Senko <Victor_senko@mail.ru>
2018-11-11 21:45:56 +0300

    add some styles and navigation switcher

--- attempts/js/script.js
+++ attempts/js/script.js
@@ -1,12 +1,16 @@
 function init() {
     $('.main__table-curent').addClass('main__table-active');
-   
+    $('.navigation__curent-task').addClass('navigation-active');
 }
 function actions() {
     $('.navigation__item').on('click', function (e) {
         $('.main__table').removeClass('main__table-active');
+        $('.navigation__item').removeClass('navigation-active');
+        $('.navigation__item').eq($(e.target).index()).addClass('navigation-active');
         $('.main__table').eq($(e.target).index()).addClass('main__table-active');
     });
+
+
 }
 init();
 actions();
