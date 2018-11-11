192d84150c96bab23d1d30a59cdf36c532fef629
Victor Senko <Victor_senko@mail.ru>
2018-11-11 23:55:05 +0300

    Add task buttons action. Modal window display

--- attempts/js/script.js
+++ attempts/js/script.js
@@ -3,14 +3,22 @@ function init() {
     $('.navigation__curent-task').addClass('navigation-active');
 }
 function actions() {
+    //navigation switcher
     $('.navigation__item').on('click', function (e) {
         $('.main__table').removeClass('main__table-active');
         $('.navigation__item').removeClass('navigation-active');
         $('.navigation__item').eq($(e.target).index()).addClass('navigation-active');
         $('.main__table').eq($(e.target).index()).addClass('main__table-active');
     });
-
-
+    //Add Task button
+    $('.open__modal-add').on('click', function () {
+        $('.modal__wrapper').css('display','block');
+    });
+    $('body').on('click', function (e) {
+        if ($(e.target).hasClass('modal__exit-button') || $(e.target).hasClass('modal__wrapper')) {
+            $('.modal__wrapper').css('display', 'none');
+        }
+    });
 }
 init();
 actions();
