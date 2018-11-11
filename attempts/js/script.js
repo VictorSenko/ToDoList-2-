3dde6fc354d8b1e6e9ad1a537c1e16a4e9584653
Victor Senko <Victor_senko@mail.ru>
2018-11-11 21:12:48 +0300

    add table switcher

--- attempts/js/script.js
+++ attempts/js/script.js
@@ -1,1 +1,12 @@
-$('.main__table-curent').addClass('main__table-active');
+function init() {
+    $('.main__table-curent').addClass('main__table-active');
+   
+}
+function actions() {
+    $('.navigation__item').on('click', function (e) {
+        $('.main__table').removeClass('main__table-active');
+        $('.main__table').eq($(e.target).index()).addClass('main__table-active');
+    });
+}
+init();
+actions();
